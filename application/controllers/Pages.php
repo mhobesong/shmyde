bfaafa3f2c81cf2758d997d36ee74c9e3ba1eb54
besongsamuel <besongsamuel@yahoo.com>
2016-01-07 01:20:57 -0500

    Updated Home page, Added about-page, contact-page, faq and review page under pages view. also added a header

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -10,10 +10,30 @@ class Pages extends CI_Controller {
         	}
         	
         	$this->load->helper('url');
+        	
+        	$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+			switch ($lang){
+    			case "fr":
+        		$current_language = "french";
+        		break;
+        		case "en":
+        		$current_language = "english";
+        		break;
+    		default:
+        		$current_language = "english";
+       			break;
+			}
+			
+			$data["application_path"] = base_url().'assets/';
+        	        	
+        	$this->lang->load('shmyde', $current_language);
 
         	$data['title'] = ucfirst($page); // Capitalize the first letter
 
+        	$this->load->view('pages/header', $data);
+
         	$this->load->view('pages/'.$page, $data);
 		}
+		
 }
 
--- application/views/Pages/home.php
+++ application/views/Pages/home.php
@@ -1,49 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>SHMYDE</title>
-    
-    <?php $application_path = base_url().'assets/';  ?>
-
-    <!-- Bootstrap -->
-    <link href="<?php echo $application_path; ?>frameworks/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo $application_path; ?>css/style.css" rel="stylesheet">
-    <link href="<?php echo $application_path; ?>frameworks/ninja-slider/ninja-slider.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
 
   <body>
-
-
-	
-
-	<div class='top-menu'>
-	
-	<div class="logo-section">
-		<p>START DESIGN</p>
-		<img src="<?php echo $application_path; ?>images/logo_shmyde.png" class="logo-image">
-		<p>GET PRODUCT</p>
-	</div>
-	
-		<ul>
-			<li><a href='#'>ABOUT US</a></li>
-			<li><a href='#'>CONTACT US</a></li>
-			<li><a href='#'>REVIEW</a></li>
-			<li><a href='#'>FAQ</a></li>
-
-		</ul>
-	</div> 
-
 
 	 <!--start ninja slider-->
 
@@ -135,9 +93,5 @@
 	</div>	
 	<!-- end first band -->	
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="<?php echo $application_path; ?>frameworks/jquery/jquery-1.11.3.min.js"></script>
-    <script src="<?php echo $application_path; ?>frameworks/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
-    <script src="<?php echo $application_path; ?>frameworks/ninja-slider/ninja-slider.js"></script>
   </body>
 </html>
