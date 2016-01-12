537439a90e7fa91f3e6fd525e69059de09a69879
Besong Moses Besong <besong@moseshost>
2016-01-12 03:53:37 +0100

    home page view

---
+++ application/views/pages/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $title; ?></title>
+    
+    <!-- Bootstrap -->
+    <link href="<?php echo $application_path; ?>frameworks/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo $application_path; ?>css/style.css" rel="stylesheet">
+    <link href="<?php echo $application_path; ?>frameworks/ninja-slider/ninja-slider.css" rel="stylesheet">
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="<?php echo $application_path; ?>frameworks/jquery/jquery-1.11.3.min.js"></script>
+    <script src="<?php echo $application_path; ?>frameworks/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo $application_path; ?>frameworks/ninja-slider/ninja-slider.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+	<div class='top-menu'>
+	
+	<div class="logo-section">
+		<p>START DESIGN</p>
+		<a href="<?php echo site_url('pages/view/home'); ?>">
+		<img src="<?php echo $application_path; ?>images/logo_shmyde.png" class="logo-image">
+		</a>
+		<p>GET PRODUCT</p>
+	</div>
+	
+		<ul>
+			<li><a href='<?php echo site_url('pages/view/about-us'); ?>'><?php echo $this->lang->line('shmyde_about_us'); ?></a></li>
+			<li><a href='<?php echo site_url('pages/view/contact-us'); ?>'><?php echo $this->lang->line('shmyde_contact_us'); ?></a></li>
+			<li><a href='<?php echo site_url('pages/view/review'); ?>'><?php echo $this->lang->line('shmyde_review'); ?></a></li>
+			<li><a href='<?php echo site_url('pages/view/faq'); ?>'><?php echo $this->lang->line('shmyde_faq'); ?></a></li>
+
+		</ul>
+	</div> 
+
+
+
+  </body>
+</html>
---
+++ application/views/pages/home.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <body>
+
+	 <!--start ninja slider-->
+
+    <div id="ninja-slider">
+
+        <div class="slider-inner">
+
+            <ul>
+
+                <li>
+
+                    <a class="ns-img" href="<?php echo $application_path; ?>frameworks/ninja-slider/img/6.jpg"></a>
+
+                    <div class="caption">
+						<b>Laborum ipsum senectus</b>
+						<div>Fugiat voluptatem Fugiat voluptatem in harum nam</div>
+					</div>
+
+                </li>
+
+                <li>
+
+                    <a class="ns-img" href="<?php echo $application_path; ?>frameworks/ninja-slider/img/1.png"></a>
+
+                    <div class="caption">
+						<b>Laborum ipsum senectus</b>
+						<div>Fugiat voluptatem Fugiat voluptatem in harum nam</div>
+					</div>
+
+                </li>
+
+                <li>
+
+                    <a class="ns-img" href="<?php echo $application_path; ?>frameworks/ninja-slider/img/1.jpg"></a>
+
+                    <div class="caption">
+						<b>Laborum ipsum senectus</b>
+						<div>Fugiat voluptatem Fugiat voluptatem in harum nam</div>
+					</div>
+
+                </li>
+
+                <li>
+
+                    <a class="ns-img" href="<?php echo $application_path; ?>frameworks/ninja-slider/img/5.jpg"></a>
+
+                    <div class="caption">
+						<b>Laborum ipsum senectus</b>
+						<div>Fugiat voluptatem Fugiat voluptatem in harum nam</div>
+					</div>
+
+                </li>
+
+                <li>
+
+                    <a class="ns-img" href="<?php echo $application_path; ?>frameworks/ninja-slider/img/3.jpg"></a>
+
+                    <div class="caption">
+						<b>Laborum ipsum senectus</b>
+						<div>Fugiat voluptatem Fugiat voluptatem in harum nam</div>
+					</div>
+
+                </li>
+
+            </ul>
+
+            <div class="navsWrapper">
+
+                <div id="ninja-slider-prev"></div>
+
+                <div id="ninja-slider-next"></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!--end ninja slider-->	
+	
+	<div class='start-customizing'>
+		<a href='#'><img src='<?php echo $application_path; ?>images/start-customizing.png'></a>
+	</div>
+
+	<!-- first band -->
+	<div class='middle-band'>
+		<p>CUSTOMER SERVICE 24 / 7</P>
+		<p><b>info @ shmyde.com</b></P>
+	</div>	
+	<!-- end first band -->	
+
+	
+	<!-- Statistics Section -->
+	<div class='stat-section'>
+
+
+		<div class='container statistics'>
+				<div class='row'>
+				
+					<div class='col-md-2 col-sm-3'>
+						<p>Tailors</p>
+						<img src='<?php echo $application_path; ?>/images/tailors-stat.png'/>
+						<p><b>43</b><p>
+					</div>
+					
+					<div class='col-md-2 col-sm-3'>
+						<p>Testimonials</p>
+						<img src='<?php echo $application_path; ?>/images/testimonials.png'/>
+						<p><b>175</b><p>
+					</div>
+					
+					
+					<div class='col-md-2 col-sm-3'>
+						<p>Deliveries</p>
+						<img src='<?php echo $application_path; ?>/images/deliveries.png'/>
+						<p><b>358</b><p>
+					</div>
+					
+					
+					<div class='col-md-6 col-sm-3'></div>
+					
+				</div>
+				
+				<div class='row'>
+				
+					<div class='col-md-2 col-sm-3'>
+						<p>Tweeter</p>
+						<img src='<?php echo $application_path; ?>/images/tweeter.png'/>
+						<p><b>43</b><p>
+					</div>
+					
+					<div class='col-md-2 col-sm-3'>
+						<p>Facebook</p>
+						<img src='<?php echo $application_path; ?>/images/facebook.png'/>
+						<p><b>175</b><p>
+					</div>
+					
+					
+					<div class='col-md-2 col-sm-3'>
+						
+					</div>
+					
+					
+					<div class='col-md-6 col-sm-3'></div>
+					
+				</div>
+				
+			</div>
+	</div>
+	<!-- end Statistics Section -->
+
+
+	<!-- second band -->
+	<div class='middle-band'>
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-3 col-sm-3 col-xs-12'></div>
+				<div class='col-md-3 col-sm-3 col-xs-12'>
+					<P>FOR ONLY $ 125</P>
+					<p><b>DESIGN IT AND WE DELIVER</b></p>
+				</div>
+				<div class='col-md-3 col-sm-3 col-xs-12'>
+					<a href='#'>
+						<img src='<?php echo $application_path; ?>/images/how-it-works.png'/>
+					</a>
+				</div>
+				<div class='col-md-3 col-sm-3 col-xs-12'></div>
+			</div>
+		</div>
+	</div>	
+	<!-- end second band -->	
+
+	<!-- models section -->
+	<div class='models'>
+		<div class='bg-video'>
+				<video muted loop autoplay>
+				  <source src="<?php echo $application_path; ?>/videos/Model Tailor WorkShop.mp4" type="video/mp4">
+				  <source src="<?php echo $application_path; ?>/videos/Model Tailor WorkShop.ogg" type="video/ogg">
+				  Your browser does not support the video tag.
+				</video>	
+		</div>
+
+		<div class='offers'>
+
+			<p>
+				<a href='#'><img src='<?php echo $application_path; ?>/images/models/men-suits.png' /></a>
+				<a href='#'><img src='<?php echo $application_path; ?>/images/models/women-suits.png' /></a>
+				<a href='#'><img src='<?php echo $application_path; ?>/images/models/jeans.png' /></a>
+				<a href='#'><img src='<?php echo $application_path; ?>/images/models/t-shirts.png' /></a>
+			</p>
+
+			<p>
+				
+				<a href='#'><img src='<?php echo $application_path; ?>/images/models/popular.png' /></a>
+			</p>
+
+		</div>
+
+	</div>
+	<!-- end models section --->	
+
+	<!-- footer section -->
+		<div class='container footer'>
+			<div class='row'>
+				<div class='col-md-4 col-sm-4'>
+					<ul>
+						<li><a href='#'>Custom Shrits</a></li>
+						<li><a href='#'>Custom T-Shirts</a></li>
+						<li><a href='#'>Custom Overcoats</a></li>
+						<li><a href='#'>Custom Blauses</a></li>
+						<li><a href='#'>Custom Suits</a></li>
+						<li><a href='#'>Custom 3PC Suits</a></li>
+						<li><a href='#'>Custom Vests</a></li>
+						<li><a href='#'>Custom Pants</a></li>
+						<li><a href='#'>Custom Jeans</a></li>
+					</ul>
+				</div>
+				<div class='col-md-4 col-sm-4'>
+					<ul>
+						<li><a href='#'>Home</a></li>
+						<li><a href='#'>About Us</a></li>
+						<li><a href='#'>Our Mission</a></li>
+						<li><a href='#'>Terms and Conditions</a></li>
+						<li><a href='#'>Privacy Policy</a></li>
+						<li><a href='#'>FAQ</a></li>
+						<li><a href='#'><p><img src='<?php echo $application_path; ?>/images/paypal.png' /></p></a></li>
+					</ul>
+
+				</div>
+				<div class='col-md-4 col-sm-4'>
+					<ul>
+						<li>Tieocom ajder saerset  sworuf Street, Yaounde Cameroon</li>
+						<li>P.O.Box : 9405 Yaounde Cameroon</li>
+						<li>Tel : +237 543 756 789</li>
+						<li>Email : info@shmyde.com</li>
+						<li>
+							<div class="input-group">
+							  <input type="text" class="form-control" placeholder="Email Subscribtion">
+							  <span class="input-group-btn">
+								<button class="btn btn-danger" type="button">OK</button>
+							  </span>
+							</div><!-- /input-group -->	
+						</li>
+					</ul>
+
+				</div>
+			</div>	
+
+			<div class='row'>
+				<div class='col-md-12 col-sm-12 col-xs-12'>
+					<p class='powered-by'>
+						Powered by Shmyde Copyright <?= date('Y'); ?> <a href='#'>Shmyde.com</a>. All Rights Reserved
+					</p>
+				</div>
+			</div>
+		</div>
+	<!-- end footer section -->
+  </body>
+</html>
