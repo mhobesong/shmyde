3317dde26b32abbc103ea4567df3b3f9f574d8ad
mhobesong <mosbesong@gmail.com>
2016-01-26 01:22:11 +0100

    fix footer display

--- application/views/pages/footer.php
+++ application/views/pages/footer.php
@@ -1,52 +1,52 @@
 <!-- footer section -->
- -		<div class='container footer'>
- -			<div class='row'>
- -				<div class='col-md-4 col-sm-4'>
- -					<ul>
- -						<li><a href='#'>Custom Shrits</a></li>
- -						<li><a href='#'>Custom T-Shirts</a></li>
- -						<li><a href='#'>Custom Suits</a></li>
- -						<li><a href='#'>Custom 3PC Suits</a></li>
- -						<li><a href='#'>Custom Jackets</a></li>
- -						<li><a href='#'>Custom Blouses</a></li>
- -					</ul>
- -				</div>
- -				<div class='col-md-4 col-sm-4'>
- -					<ul>
- -						<li><a href='#'>Home</a></li>
- -						<li><a href='#'>About Us</a></li>
- -						<li><a href='#'>Our Mission</a></li>
- -						<li><a href='#'>Terms and Conditions</a></li>
- -						<li><a href='#'>Privacy Policy</a></li>
- -						<li><a href='#'>FAQ</a></li>
- -					</ul>
- -
- -				</div>
- -				<div class='col-md-4 col-sm-4'>
- -					<ul>
- -						<li>Tieocom ajder saerset  sworuf Street, Yaounde Cameroon</li>
- -						<li>P.O.Box : 9405 Yaounde Cameroon</li>
- -						<li>Tel : +237 543 756 789</li>
- -						<li>Email : info@shmyde.com</li>
- -						<li>
- -							<div class="input-group">
- -							  <input type="text" class="form-control" placeholder="Email Subscribtion">
- -							  <span class="input-group-btn">
- -								<button class="btn btn-danger" type="button">OK</button>
- -							  </span>
- -							</div><!-- /input-group -->	
- -						</li>
- -					</ul>
- -
- -				</div>
- -			</div>	
- -
- -			<div class='row'>
- -				<div class='col-md-12 col-sm-12 col-xs-12'>
- -					<p class='powered-by'>
- -						Powered by Shmyde Copyright <?= date('Y'); ?> <a href='#'>Shmyde.com</a>. All Rights Reserved
- -					</p>
- -				</div>
- -			</div>
- -		</div>
- -	<!-- end footer section -->
+ 		<div class='container footer'>
+ 			<div class='row'>
+ 				<div class='col-md-4 col-sm-4'>
+ 					<ul>
+ 						<li><a href='#'>Custom Shrits</a></li>
+ 						<li><a href='#'>Custom T-Shirts</a></li>
+ 						<li><a href='#'>Custom Suits</a></li>
+ 						<li><a href='#'>Custom 3PC Suits</a></li>
+ 						<li><a href='#'>Custom Jackets</a></li>
+ 						<li><a href='#'>Custom Blouses</a></li>
+ 					</ul>
+ 				</div>
+ 				<div class='col-md-4 col-sm-4'>
+ 					<ul>
+ 						<li><a href='#'>Home</a></li>
+ 						<li><a href='#'>About Us</a></li>
+ 						<li><a href='#'>Our Mission</a></li>
+ 						<li><a href='#'>Terms and Conditions</a></li>
+ 						<li><a href='#'>Privacy Policy</a></li>
+ 						<li><a href='#'>FAQ</a></li>
+ 					</ul>
+ 
+ 				</div>
+ 				<div class='col-md-4 col-sm-4'>
+ 					<ul>
+ 						<li>Tieocom ajder saerset  sworuf Street, Yaounde Cameroon</li>
+ 						<li>P.O.Box : 9405 Yaounde Cameroon</li>
+ 						<li>Tel : +237 543 756 789</li>
+ 						<li>Email : info@shmyde.com</li>
+ 						<li>
+ 							<div class="input-group">
+ 							  <input type="text" class="form-control" placeholder="Email Subscribtion">
+ 							  <span class="input-group-btn">
+ 								<button class="btn btn-danger" type="button">OK</button>
+ 							  </span>
+ 							</div><!-- /input-group -->	
+ 						</li>
+ 					</ul>
+ 
+ 				</div>
+ 			</div>	
+ 
+ 			<div class='row'>
+ 				<div class='col-md-12 col-sm-12 col-xs-12'>
+ 					<p class='powered-by'>
+ 						Powered by Shmyde Copyright <?= date('Y'); ?> <a href='#'>Shmyde.com</a>. All Rights Reserved
+ 					</p>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	<!-- end footer section -->
