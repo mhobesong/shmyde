bdfebf209a91ce9b4122b9d0a515b64eca5e1c47
mhobesong <mosbesong@gmail.com>
2016-02-13 02:17:52 +0100

    Design page base view ...

--- application/views/design.php
+++ application/views/design.php
@@ -3,20 +3,77 @@
 	<div id='' class='container'>
 		<div id='' class='row'>
 			<div id='' class='col-md-2 col-sm-2 design-menu'>
-				<div id='' class='design-menu-header'></div>
+				<div id='' class='design-menu-header'>Design Menu</div>
+
+				<ol>
+					<li><a href="#">Fabric</a></li>
+					<li><a href="#">Fabric</a></li>
+					<li><a href="#">Color</a></li>
+					<li><a href="#">Measurements</a></li>
+				</ol>
 				
 			</div>
 			<div id='' class='col-md-10 col-sm-10'>
 				<div id='' class='row'>
-					<div id='' class='col-md-2 col-sm-2'>
-						<div id='' class='design-sub-menu'></div>
+					<div id='' class='col-md-2 col-sm-2' style='padding:0;'>
+						<div id='' class='design-sub-menu'>
+							<ul>
+								<li><a href="#">Contrast</a></li>
+								<li><a href="#">Button</a></li>
+								<li><a href="#">Monogram</a></li>
+							</u>
+						</div>
 					</div>
 					<div id='' class='col-md-8 col-sm-8' style='padding:0'>
-						<div id='' class='design-preview'></div>
+						<div id='' class='design-preview'><img src="<?= base_url("assets/images/design/shirt/base_shirt.png") ?>" /></div>
 					</div>
-					<div id='' class='col-md-2 col-sm-2'></div>
+					<div id='' class='col-md-2 col-sm-2 design-side'>
+						<h4>SIDES</h4>
+
+						<ul>
+							<li class='active' ><a href="#">
+								<h5>Front</h5>
+								<img src="<?= base_url("assets/images/design/front.png"); ?>">
+							</a></li>
+							<li><a href="#">
+								<h5>Back</h5>
+								<img src="<?= base_url("assets/images/design/back.png"); ?>">
+							</a></li>
+
+						</ul>
+					</div>
 				</div>
-				<div id='' class='row design-fabrique'></div>
+				<div class='fabrique-heading'>Chose Your Fabrique</div>
+				<div id='' class='row design-fabrique'>
+					<div class="col-sm-2 col-md-2 design-part">
+						<img src="<?= base_url("assets/images/design/shirt/part-preview/collar-button.png"); ?>">
+					</div>
+					<div class="col-sm-8 col-md-8">
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/default-white.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/1.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/2.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/3.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/4.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/5.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/6.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/7.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/8.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/9.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/10.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/11.png"); ?>"></a>
+						<a href="#"><img class='active' src="<?= base_url("assets/images/design/fabriques/12.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/13.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/14.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/15.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/16.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/17.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/18.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/19.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/20.png"); ?>"></a>
+						<a href="#"><img src="<?= base_url("assets/images/design/fabriques/21.png"); ?>"></a>
+					</div>
+					<div class="col-sm-2 col-md-2"></div>
+				</div>
 			</div>
 		</div>
 	</div>
